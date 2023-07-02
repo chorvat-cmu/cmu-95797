54a86b93433a8fe58ac33b0e2473592226e11aaa
chorvat-cmu <138394705+chorvat-cmu@users.noreply.github.com>
2023-07-02 16:19:40 -0400

    Add files via upload

---
+++ import_csv_duckdb.py
@@ -0,0 +1,14 @@
+import duckdb
+import glob
+
+conn = duckdb.connect('/duckdb_week1')
+
+csv_directory = 's3data/data/*.csv'
+
+for csv_file in glob.glob(csv_directory):
+    data = conn.execute(f"SELECT * FROM read_csv_auto('{csv_file}')").fetchdf()
+    if conn.execute("SELECT 1 FROM pragma_table_info('my_table') LIMIT 1").fetchone() is None:
+        conn.register('my_table', data)
+    else:
+        conn.execute("INSERT INTO my_table SELECT * FROM read_csv_auto(?)", (csv_file,))
+
